06f90b6a93e7a0f140787d763c0690a265d9d38d
akash3927 <akashkothawade77@gmail.com>
2021-07-10 23:30:30 +0530

    new commit

---
+++ variablesndatatypes.py
@@ -0,0 +1,41 @@
+#variables and data types
+message="hello akash"
+print(message)
+#strings
+#1
+print(message.title())
+#2
+print(message.upper())
+#3
+print(message.lower())
+#combine or concating strings
+firstname="hello"
+lastname=" akash"
+fullname=firstname + lastname
+print(fullname)
+#/t=use for giving tab
+#/n=use for creating new line
+data=" hi "
+print(data.rstrip())
+print(data.lstrip())
+print(data.strip())
+#numbers
+#integers
+a=2+3
+b=3-2
+c=3*2
+d=3/2
+e=3**2
+f=3**3
+g=3//2
+h= 2 + 3*4
+i= (2+3)*4
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
